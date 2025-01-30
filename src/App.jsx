d67a41252256ac505ac1d66df0fd4a7b998cfcd1
Zura Shukakidze <zurashukakidze85@gmail.com>
2025-01-30 13:56:16 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,67 @@
+import { useState } from "react"
+
 function App() {
 
+  const[board, setBoard] = useState(Array(9).fill(null))
   
+  const [isXTurn, setIsXTurn] = useState(true)
+
+  const winningCombinations = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], // rows
+
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], // columns
+
+    [0, 4, 8], 
+    [2, 4, 6] // diagonals
+  ]
+
+  function getWinner(squares){
+
+    for(let combination of winningCombinations){
+      const [a,b,c] = combination
+
+      if(
+        squares [a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ){
+        return squares[a]
+      }
+    }
+    return null
+  }
+
+  function handleSquareClick(index){
+    if(board[index] || getWinner(board))
+      return
+    
+    const updatedBoard = [...board]
+    updatedBoard[index] = isXTurn ? 'X' : 'O'
+
+    setBoard(updatedBoard)
+    setIsXTurn(!isXTurn)
+  }
+
+  function getGameStatus(){
+    const winner = getWinner(board)
+
+    if(winner){
+      return `Winner: ${winner}`
+    } else if(board.every(square => square)){
+      return 'Draw'
+    } else {
+      return `Next player: ${isXTurn ? 'X' : 'O'}`
+  }
+
+  function resetGame () {
+    setBoard(Array(9).fill(null))
+    setIsXTurn(true)
+  }
+
 
   return (
     <div className="min-h-screen bg-slate-950 flex items-center
