1d8bde7b419c32a8820e9a2f295fef59a303d326
Zura Shukakidze <zurashukakidze85@gmail.com>
2025-01-30 13:33:49 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="min-h-screen bg-slate-900 items-center
+    justify-center">
+
+      <div className="w-full max-w-[400px} mx-5"></div>
+
+    </div>
+  )
+}
+
+export default App
